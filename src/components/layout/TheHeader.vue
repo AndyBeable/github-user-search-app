253af5b80a02c60df88e164c48ace12b6d99fb01
AndyBeable <andy.beable@gmail.com>
2021-10-01 16:40:44 +0100

    header initial

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,3 +1,26 @@
 <template>
-  <h1>Dev Finder</h1>
+  <div class="header">
+    <div class="header-title">
+      <h1>devfinder</h1>
+    </div>
+    <div class="header-theme">
+      <div class="theme-light">
+        <h2 class="theme-light-text">Light</h2>
+        <svg></svg>
+      </div>
+      <div class="theme-dark">
+        <h2 class="theme-dark-text">Dark</h2>
+        <svg></svg>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.header {
+  display: flex;
+}
+.header-theme {
+  display: flex;
+}
+</style>
